6d02e6729a98c9bea3077dea5ec628c0cf140852
Felipe Rezende <frezende21@gmail.com>
2025-03-14 23:13:41 -0300

    ADD: Ajuste de arquivos TF para autenticar na esteira

--- OIDC-Terraform-GH-AZ/main.tf
+++ OIDC-Terraform-GH-AZ/main.tf
@@ -1,7 +1,33 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>2.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = azurerm_resource_group.oidc.name  # Nome do Resource Group onde o Storage Account está
+    storage_account_name = azurerm_storage_account.oidc.name # Nome do Storage Account
+    container_name       = azurerm_storage_container.Container.name        # Nome do Container criado
+    key                  = "terraform.tfstate"       # Nome do arquivo de estado
+  }
 }
 
+provider "azurerm" {
+  features {
+    
+  }
+}
 # Cria uma storage account para armazenar o estado
 resource "random_integer" "oidc" {
   min = 10000
