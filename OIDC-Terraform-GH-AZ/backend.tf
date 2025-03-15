1792467ef770a37a56297305ddea04fcfa0aaeae
Felipe Rezende <frezende21@gmail.com>
2025-03-15 00:43:04 -0300

    ajuste de pipe

--- OIDC-Terraform-GH-AZ/backend.tf
+++ OIDC-Terraform-GH-AZ/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
    
-    key = "terraform.tfstate"       # Nome do arquivo de estado
+    key = "terraform.tfstate"
     use_oidc = true
     use_azuread_auth = true
   }
--- OIDC-Terraform-GH-AZ/main.tf
+++ OIDC-Terraform-GH-AZ/main.tf
@@ -3,10 +3,11 @@ provider "azurerm" {
   features {
   }
   use_oidc = true
+
 }
 
 resource "azurerm_resource_group" "oidc" {
-  name     = "oidc-${random_integer.oidc.result}"
+  name     = "rg-oidc-aplicado"
   location = var.azure_region
 }
 
